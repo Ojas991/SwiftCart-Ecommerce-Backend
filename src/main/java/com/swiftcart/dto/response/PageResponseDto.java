7a9e502f169144541275ddfc45cf251c75658363
Ojesh <ojasbisen624@gmail.com>
2026-07-23 00:17:54 +0530

    Add Swagger/OpenAPI documentation for PageResponseDto.

--- src/main/java/com/swiftcart/dto/response/PageResponseDto.java
+++ src/main/java/com/swiftcart/dto/response/PageResponseDto.java
@@ -4,26 +4,70 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Generic paginated response containing records and pagination details")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PageResponseDto<T> {
+	
+	@Schema(description = "List of records in the current page")
 	private List<T> content;
+	
+    @Schema(
+            description = "Current page number (0-based index)",
+            example = "0"
+        )
 	private int pageNumber;
+    
+    @Schema(
+            description = "Number of records per page",
+            example = "10"
+        )
 	private int pageSize;
+    
+    @Schema(
+            description = "Total number of records",
+            example = "125"
+        )
 	private long totalElements;
+    
+    @Schema(
+            description = "Total number of pages",
+            example = "13"
+        )
 	private int totalPages;
+    
+    @Schema(
+            description = "Indicates whether this is the first page",
+            example = "true"
+        )
 	private boolean first;
+    
+    @Schema(
+            description = "Indicates whether this is the last page",
+            example = "false"
+        )
 	private boolean last;
+    
+    @Schema(
+            description = "Indicates whether a next page exists",
+            example = "true"
+        )
 	private boolean hasNext;
+    
+    @Schema(
+            description = "Indicates whether a previous page exists",
+            example = "false"
+        )
 	private boolean hasPrevious;
 
 	// Static method <----Static method ---->
